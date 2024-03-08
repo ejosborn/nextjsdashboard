c3c2e5d3a5113344ca0461913c24fe4b4af51810
Ellis Osborn <ejosborn@asu.edu>
2024-03-08 10:11:59 -0800

    created revenueChart card with styling

--- app/ui/dashboard/revenueCharts.tsx
+++ app/ui/dashboard/revenueCharts.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { fetchRevenue } from '@/app/lib/data';
 import { roboto } from '../fonts';
 import { generateYAxis } from '@/app/lib/utils';
+import { revenue } from '@/app/lib/placeholderdata';
 
 const RevenueCharts = async () => {
 	const rev = await fetchRevenue();
@@ -13,8 +14,38 @@ const RevenueCharts = async () => {
 			<h2
 				className={`${roboto.className} mb-4 text-xl md:text-2xl`}
 			>
-				Revenue
+				Recent Revenue
 			</h2>
+			<div className='rounded-xl bg-gray-50 p-4'>
+				<div className='custom-grid-column mt-0 grid grid-cols-12 items-end gap-2 rouded-md bg-white p-4 md:gap-4 text-gray-400'>
+					<div
+						className='mb-6 hidden sm:flex flex-col justify-between text-sm text-gray-400'
+						style={{ height: `${chartHeight}px` }}
+					>
+						{yAxisLabel.map((label) => (
+							<p key={label}>{label}</p>
+						))}
+					</div>
+					{revenue.map((data) => (
+						<div
+							key={data.month}
+							className='flex flex-col items-center gap-1.5 text-gray-400'
+						>
+							<div
+								className='w-full rounded-md bg-indigo-200'
+								style={{
+									height: `${
+										(chartHeight / topLabel) * data.revenue
+									}px`
+								}}
+							></div>
+							<p className='-rotate-90 text-sm text-gray-400 sm:rotate-0'>
+								{data.month}
+							</p>
+						</div>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 };
